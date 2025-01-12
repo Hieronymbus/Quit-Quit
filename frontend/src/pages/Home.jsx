a2a9994a4c0cb0eb86f9d01803956379efe15746
alex birdsall <birdsallalexander1990@gmail.com>
2025-01-13 12:00:39 +1300

    fronted: added functionality for onclick sto navigate to correct pages

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,21 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
+
+  const navigate = useNavigate()
+
+  const handleClickLogin = () => {
+      navigate("/login")
+  }
+
+  const handleClickRegister = () => {
+      navigate("/register")
+  }
+
+  const handleClickAbout = () => {
+      navigate("/about")
+  }
 
 
   return (
@@ -20,15 +35,23 @@ const Home = () => {
         </h2>
       </div>
       <div
-        className='w-1/4 aspect-square rounded-xl bg-purple-700 grid grid-cols-3'
+        className='w-1/3 aspect-square rounded-xl bg-purple-700 grid grid-cols-3'
       >
-        <div class=" bg-purple-700 rounded-xl"></div>
-        <div class=" bg-purple-700 flex justify-center items-center hover:cursor-pointer hover:bg-purple-500">
-          ABOUT
+        <div 
+          className=" bg-purple-700 rounded-xl flex justify-center items-center hover:cursor-pointer hover:bg-purple-500"
+          onClick={handleClickLogin}
+        >
+          LOGIN
         </div>
-        <div class=" bg-purple-700 rounded-xl"></div>
-        <div class=" bg-purple-700 "></div>
-        <div class=" bg-purple-700 flex justify-center items-center">
+        <div className=" bg-purple-700"></div>
+        <div
+          className=" bg-purple-700 rounded-xl flex justify-center items-center hover:cursor-pointer hover:bg-purple-500"
+          onClick={handleClickRegister}
+        >
+          Register
+        </div>
+        <div className=" bg-purple-700 "></div>
+        <div className=" bg-purple-700 flex justify-center items-center">
           <img 
             src='./assets/rb_35535.png'
             className='w-full h-full object-contain'
@@ -36,14 +59,15 @@ const Home = () => {
           />
           
         </div>
-        <div class=" bg-purple-700"></div>
-        <div class=" bg-purple-700 rounded-xl flex justify-center items-center hover:cursor-pointer hover:bg-purple-500">
-          LOGIN
+        <div className=" bg-purple-700"></div>
+        <div className=" bg-purple-700 rounded-xl"></div>
+        <div 
+          className=" bg-purple-700 flex justify-center items-center hover:cursor-pointer hover:bg-purple-500"
+          onClick={handleClickAbout}
+        >
+          ABOUT
         </div>
-        <div class=" bg-purple-700"></div>
-        <div class=" bg-purple-700 rounded-xl flex justify-center items-center hover:cursor-pointer hover:bg-purple-500">
-          Register
-        </div>
+        <div className=" bg-purple-700 rounded-xl"></div>
       </div>
 
       
